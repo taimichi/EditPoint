5c142f99813423810a640653aa596044a732095a
kokoA7V <oktakeru@icloud.com>
2024-07-04 12:48:39 +0900

    プレイヤーと動く床仮実装

--- EditPoint/Assets/kokoA7V/Scripts/PlayerController.cs
+++ EditPoint/Assets/kokoA7V/Scripts/PlayerController.cs
@@ -7,14 +7,19 @@ public class PlayerController : MonoBehaviour
     GroundChecker gc;
     Rigidbody2D rb;
 
-    MoveController mc;
+    //MoveController mc;
+
+    Vector3 scale;
+
+    int inputLR = 0;
+    Vector3 movePos;
 
     void Start()
     {
         gc = GetComponent<GroundChecker>();
         rb = GetComponent<Rigidbody2D>();
 
-        mc = new MoveController(rb);
+        //mc = new MoveController(rb);
 
         gc.InitCol();
     }
@@ -23,24 +28,52 @@ public class PlayerController : MonoBehaviour
     {
         gc.CheckGround();
 
-        mc.MoveLR();
+        //mc.MoveLR();
 
         PlayerInput();
+
+
+
+        movePos = this.transform.position;
+        if (inputLR != 0)
+        {
+            movePos.x += inputLR * 0.1f;
+        }
+        this.transform.position = movePos;
+
+        //Debug.Log(rb.velocity);
+
+        //scale = this.transform.localScale;
+        //if (mc.inputLR != 0)
+        //{
+        //    scale.x = Mathf.Abs(scale.x) * mc.inputLR;
+        //}
+        //this.transform.localScale = scale;
+
+        scale = this.transform.localScale;
+        if (inputLR != 0)
+        {
+            scale.x = Mathf.Abs(scale.x) * inputLR;
+        }
+        this.transform.localScale = scale;
     }
 
     void PlayerInput()
     {
         if (Input.GetKey(KeyCode.D))
         {
-            mc.inputLR = 1;
+            //mc.inputLR = 1;
+            inputLR = 1;
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            mc.inputLR = -1;
+            //mc.inputLR = -1;
+            inputLR = -1;
         }
         else
         {
-            mc.inputLR = 0;
+            //mc.inputLR = 0;
+            inputLR = 0;
         }
     }
 
