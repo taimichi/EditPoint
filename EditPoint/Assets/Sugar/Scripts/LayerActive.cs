83b540dd491ed2e77a77cf2a499780fc1b12dc0e
DESKTOP-67MT2K8\user <nkc20226030@gmail.com>
2024-07-11 13:47:22 +0900

    レイヤーパネルの場所変更+ボタンで出るように

---
+++ EditPoint/Assets/Sugar/Scripts/LayerActive.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerActive : MonoBehaviour
+{
+    // 動かすターゲットUI
+    [SerializeField]
+    RectTransform TargetRct;
+
+    // 初期の座標値
+    [SerializeField]
+    Vector2 startPos = new Vector2(0, 0);
+
+    // 目標の座標
+    [SerializeField]
+    Vector2 TargetPos = new Vector2(0, 0);
+
+    // 座標変更時の速度
+    [SerializeField]
+    float spdX = 0, spdY = 0;
+
+    // アニメーション状態
+    int state = 3;
+
+    // クラス
+    ClassUIAnim UAnim;
+
+    bool click = false;
+
+    void Start()
+    {
+        // インスタンス生成
+        UAnim = new ClassUIAnim();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (click)
+        {
+            UICONTROLL();
+        }
+    }
+
+    private void UICONTROLL()
+    {
+        switch (state)
+        {
+            case 1: // グループ非表示
+                if (TargetRct.anchoredPosition.x <= startPos.x)
+                {
+                    TargetRct = UAnim.anim_PosChange(TargetRct, -spdX, spdY);
+                }
+                else
+                {
+                    TargetRct.anchoredPosition = startPos;
+                    state = 10; //　処理の終了
+                }
+                break;
+            case 3: // グループ表示
+                if (TargetRct.anchoredPosition.x>= TargetPos.x)
+                {
+                    TargetRct = UAnim.anim_PosChange(TargetRct, spdX, spdY);
+                }
+                else
+                {
+                    TargetRct.anchoredPosition = TargetPos;
+                    state = 10; //　処理の終了
+                }
+                break;
+            case 10:
+                click = false;
+                state = TargetRct.anchoredPosition == startPos ? 3 : 1;
+                break;
+        }
+    }
+    public void LAYERBUTTON()
+    {
+        click = true;
+    }
+}
