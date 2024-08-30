932751562519b90cb33023534102a9e7c7c46a65
kokoA7V <oktakeru@icloud.com>
2024-08-31 03:57:40 +0900

    BlowerController方向転換スクリプト作成

---
+++ EditPoint/Assets/kokoA7V/Scripts/BlowerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlowerController : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask lm;
+
+    [SerializeField]
+    Blower nowBlower;
+
+    [SerializeField]
+    int nowDir = 0;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            nowBlower = null;
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            RaycastHit2D rh = Physics2D.Linecast(mousePos, mousePos, lm);
+            if (rh.collider != null)
+            {
+                if (rh.collider.transform.parent.gameObject.TryGetComponent<Blower>(out var _blower))
+                {
+                    nowBlower = _blower;
+                    nowDir = (int)nowBlower.dir;
+                }
+            }
+        }
+
+
+        nowDir += (int)Input.mouseScrollDelta.y;
+        if (nowDir > 3)
+        {
+            nowDir = 0;
+        }
+        else if (nowDir < 0)
+        {
+            nowDir = 3;
+        }
+
+        nowBlower.dir = nowDir;
+    }
+}
