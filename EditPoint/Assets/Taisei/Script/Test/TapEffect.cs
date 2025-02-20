39a6e375438a34707e9c7f6bcd2aaf1274df989e
DESKTOP-67MT2K8\user <nkc20226030@gmail.com>
2025-02-20 09:43:10 +0900

    シーンにエフェクトの設定

--- EditPoint/Assets/Taisei/Script/Test/TapEffect.cs
+++ EditPoint/Assets/Taisei/Script/Test/TapEffect.cs
@@ -11,9 +11,13 @@ public class TapEffect : MonoBehaviour
 
     void Update()
     {
-
         pos = _camera.ScreenToWorldPoint(Input.mousePosition);
         pos.z = 1;
         Trail.transform.position = pos;
+
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            Instantiate(TapEffectPrefab, pos, Quaternion.identity);
+        }
     }
 }
