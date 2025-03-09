dc52ae77375c48cc49eaa3a1b6dd4f855e79428d
kokoA7V <oktakeru@icloud.com>
2025-03-10 03:50:23 +0900

    動くエネミーの再生時間対応、接地不可エリア実装

--- EditPoint/Assets/kokoA7V/Scripts/Gimmick/Enemy/MoveEnemyController.cs
+++ EditPoint/Assets/kokoA7V/Scripts/Gimmick/Enemy/MoveEnemyController.cs
@@ -20,14 +20,29 @@ public class MoveEnemyController : MonoBehaviour
 
     private void Start()
     {
-        this.transform.position = pathPosition[nowPath];
-        timer = pathTime[nowPath];
-
-        pathPosition.Add(Vector3.zero);
-        pathTime.Add(0);
+        PathReset();
     }
 
     private void Update()
+    {
+        if (GameData.GameEntity.isPlayNow)
+        {
+            EnemyMove();
+        }
+
+        if (GameData.GameEntity.isTimebarReset)
+        {
+            PathReset();
+        }
+    }
+
+    private void PathReset()
+    {
+        this.transform.position = pathPosition[nowPath];
+        timer = pathTime[nowPath];
+    }
+
+    private void EnemyMove()
     {
         // 時間管理
         timer -= Time.deltaTime * speed;
@@ -36,24 +51,31 @@ public class MoveEnemyController : MonoBehaviour
         Vector3 movePos = this.transform.position;
 
         // 方向を計算
-        Vector3 dist = pathPosition[nowPath + 1] - pathPosition[nowPath];
-        if (nowPath + 1 == pathPosition.Count - 1)
+        Vector3 dist;
+        if (nowPath == pathPosition.Count - 1)
         {
             dist = pathPosition[0] - pathPosition[nowPath];
+        }
+        else
+        {
+            dist = pathPosition[nowPath + 1] - pathPosition[nowPath];
         }
         Vector3 moveSpeed = dist / pathTime[nowPath];
 
         movePos += moveSpeed * Time.deltaTime * speed;
-        
+
         this.transform.position = movePos;  // 座標更新
 
         // パス更新
         if (timer <= 0)
         {
-            nowPath++;
             if (nowPath == pathPosition.Count - 1)
             {
                 nowPath = 0;
+            }
+            else
+            {
+                nowPath++;
             }
             timer = pathTime[nowPath];
         }
